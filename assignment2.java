fcc5daaed26ec36deff4bf8f0a9733396da75d95
Manish-1106 <83824292+Manish-1106@users.noreply.github.com>
2021-05-07 19:21:54 +0530

    Add files via upload

---
+++ assignment2.java
@@ -0,0 +1,17 @@
+public class assignment2
+{
+public static void main(String []args)
+{
+String S1=args[0];
+String S2=args[1];
+
+int i =Integer.parseInt(S1);
+int J =Integer.parseInt(S2);
+int k = i+J;
+
+System.out.println("first argument :"+i);
+System.out.println("second argument :"+J);
+System.out.println("third argument :"+k);
+}
+}
+
